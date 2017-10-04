e0423200024baea0b63173633cd4fb9388e0b977
Allison Muise <amuise@esri.com>
2017-10-04 15:05:24 -0700

    pointed to argv for config file

--- CityworksConnection/connect_to_cityworks.py
+++ CityworksConnection/connect_to_cityworks.py
@@ -439,7 +439,7 @@ if __name__ == "__main__":
 
     import sys
 
-    configfile = r'D:\CityWorks\CityworksConnection\Connect_to_Cityworks.JSON'  # sys.argv[1]
+    configfile = sys.argv[1]
 
     with open(configfile) as configreader:
         config = json.load(configreader)
